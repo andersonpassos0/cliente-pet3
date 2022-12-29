fea272dab554fcd119ccd87f0401e8d31b79abf2
andersonpassos0 <anderson.passos@yahoo.com.br>
2022-12-29 17:06:24 -0300

    implementa fluxo api

---
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteListResponse.java
@@ -0,0 +1,16 @@
+package br.com.petz.clientepet.cliente.application.api;
+
+import java.util.UUID;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class ClienteListResponse {
+	private UUID idCliente;
+	private String nomeCompleto;
+	private String cpf;
+	private String email;
+	private String celular;
+}
