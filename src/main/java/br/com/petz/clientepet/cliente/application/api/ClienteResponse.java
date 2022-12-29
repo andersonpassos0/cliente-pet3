d67878e9a86d84914407bd277a22f933d43426f3
andersonpassos0 <anderson.passos@yahoo.com.br>
2022-12-29 10:46:19 -0300

    implementa request e response

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteResponse.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteResponse.java
@@ -1,5 +1,15 @@
 package br.com.petz.clientepet.cliente.application.api;
 
+import java.util.UUID;
+
+import lombok.Value;
+
+@Value
 public class ClienteResponse {
+	private UUID idCliente;
+	private String nomeCompleto;
+	private String email;
+	private String celular;
+	private String telefone;
 
 }
