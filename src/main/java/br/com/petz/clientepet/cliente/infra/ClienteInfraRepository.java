e064bf79a68cef1b03b1b845a908172b813440c8
andersonpassos0 <anderson.passos@yahoo.com.br>
2022-12-29 16:28:02 -0300

    implementa fluxo do repository. teste ok

--- src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
@@ -1,5 +1,8 @@
 package br.com.petz.clientepet.cliente.infra;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 import br.com.petz.clientepet.cliente.application.repository.ClienteRepository;
@@ -13,6 +16,7 @@ public class ClienteInfraRepository implements ClienteRepository {
 	@Override
 	public Cliente salva(Cliente cliente) {
 		log.info("[start] ClienteInfraRepository - salva");
+		
 		log.info("[finish] ClienteInfraRepository - salva");
 		return cliente;
 	}
