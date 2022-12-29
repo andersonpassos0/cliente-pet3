adc33abe8d7fccd83c7e0b388809e4f546e60321
andersonpassos0 <anderson.passos@yahoo.com.br>
2022-12-29 10:14:27 -0300

    SEM ALTERACAO DE CODIGO - erro no index do git - #corrigido#

---
+++ src/main/java/br/com/petz/clientepet/cliente/domain/Cliente.java
@@ -0,0 +1,61 @@
+package br.com.petz.clientepet.cliente.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ìdCliente", updatable = false, unique = true, nullable = false)
+	private UUID idCliente;
+	@NotBlank
+	private String nomeCompleto;
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	private String celular;
+	private String telefone;
+	private Sexo sexo;
+	private LocalDate dataNascimento;
+	@CPF
+	@NotNull
+	private String cpf;
+	@NotNull
+	private Boolean aceitaTermos;
+
+	private LocalDateTime dataHoraDoCadastro;
+	private LocalDateTime dataHoraDaUltimaAlteracao;
+	
+	private Cliente(@NotBlank String nomeCompleto, @NotBlank @Email String email, @NotBlank String celular,
+			String telefone, Sexo sexo, LocalDate dataNascimento, @CPF @NotNull String cpf,
+			@NotNull Boolean aceitaTermos) {
+		this.nomeCompleto = nomeCompleto;
+		this.email = email;
+		this.celular = celular;
+		this.telefone = telefone;
+		this.sexo = sexo;
+		this.dataNascimento = dataNascimento;
+		this.cpf = cpf;
+		this.aceitaTermos = aceitaTermos;
+		this.dataHoraDoCadastro = LocalDateTime.now();
+	}
+}
